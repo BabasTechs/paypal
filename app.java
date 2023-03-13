6131f5db19d20540c081585777ed8f2ebe3aaadb
Babas-3 <126036519+Babas-3@users.noreply.github.com>
2023-03-13 02:27:49 -0400

    Update app.java
    app.java has updated

--- app.java
+++ app.java
@@ -1,2 +1,6 @@
 This is a paypal repository
 This is a paypal applcation for payments
+This is the way you do it
+ git fetch --> It brings changes in the code to the LocalRepo
+  git pull --> It brings changes in the code to the Working area
+  git clone --> It brings the entire code to the working area
